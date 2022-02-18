67b5df691969ee300b061da0315352888498432d
sayyid <sayyidsyafiq234@gmail.com>
2022-02-18 10:49:55 +0700

    commit dari sayyid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $Homes[0]->siswa }}</div>
                                 </div>
                                 <div class="col mr-2">
-                                    <i class="fas fa-user fa-2x text-gray-300"></i>
+                                    <i class="fas fa-user-graduate fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
